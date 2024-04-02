21d62781553ff01fc4ddfe8714f57d4c4e10f75b
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-04-02 16:57:12 -0300

    Update update_json.py

--- script/update_json.py
+++ script/update_json.py
@@ -19,10 +19,10 @@ def filter_and_save_old(data, category, output_filename):
 
 # Função para filtrar e salvar os dados em um novo arquivo JSON
 def filter_and_save(data, category, output_filename):
-    filtered_data = [item for item in data if item.get('category') == category and any(version.get('published') for version in item.get('versions'))]
+    filtered_data = [item for item in data if item.get('category') == category and any(version.get('published') for version in item.get('versions', []))]
     filtered_data = [{k: v for k, v in item.items() if k in ['fid', 'name', 'category', 'author']} for item in filtered_data]
     for item in filtered_data:
-        versions = item['versions']
+        versions = item.get('versions', [])
         if versions:
             latest_version = max(versions, key=lambda x: x['published_at'])
             item.update({k: latest_version[k] for k in ['version', 'published_at', 'file']})
