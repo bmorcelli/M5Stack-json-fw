fdc9965fa003c979f78bc7392c482643c90bc58a
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-04-02 17:03:30 -0300

    Update update_json.py

--- script/update_json.py
+++ script/update_json.py
@@ -24,19 +24,21 @@ def filter_and_save(data, category, output_filename):
     for item in filtered_data:
         versions = item.get('versions', [])
         if versions:
-            latest_version = max(versions, key=lambda x: x['published_at'])
-            filtered_item = {
-                'fid': item['fid'],
-                'name': item['name'],
-                'category': item['category'],
-                'author': item['author'],
-                'version': latest_version['version'],
-                'published_at': latest_version['published_at'],
-                'file': latest_version['file']
-            }
-            final_data.append(filtered_item)
+            latest_version = max(versions, key=lambda x: x.get('published_at', ''))
+            if 'published_at' in latest_version:
+                filtered_item = {
+                    'fid': item['fid'],
+                    'name': item['name'],
+                    'category': item['category'],
+                    'author': item['author'],
+                    'version': latest_version['version'],
+                    'published_at': latest_version['published_at'],
+                    'file': latest_version['file']
+                }
+                final_data.append(filtered_item)
     with open(output_filename, 'w') as f:
         json.dump(final_data, f, indent=2)
+
 
 # Request à API e salvando os dados em all_device_firmware.json
 response = requests.get('https://m5burner-api.m5stack.com/api/firmware')
