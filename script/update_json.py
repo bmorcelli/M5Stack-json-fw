5007f823338996db1b00a2ec2f9801be5c93985a
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-04-25 21:07:56 -0300

    Update update_json.py

--- script/update_json.py
+++ script/update_json.py
@@ -115,6 +115,8 @@ def create_filtered_file(category_name):
         item.pop('tags', None)
         item.pop('github', None)
         item.pop('download', None)
+        item.pop('_id', None)
+        item.pop('category', None)
 
     with open(f"{temp_folder}{category_name}.json", 'w') as file:
         json.dump(filtered_data, file)
