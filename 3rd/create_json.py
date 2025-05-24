06adc09abde9207cd06cc719edc9787c8f359486
Pirata <bmorcelli@gmail.com>
2025-05-24 14:17:48 -0300

    json builder

--- 3rd/create_json.py
+++ 3rd/create_json.py
@@ -50,7 +50,7 @@ for filename in os.listdir(input_folder):
 
     for item in data:
         for version in item['versions']:
-            if 'spiffs' in version:
+            if 's' in version:
                 print(f"{item['name']} - {version['version']} - Ok", flush=True)
             else:
                 print(f"{item['name']} - {version['version']} - {version['file']}", flush=True)
@@ -59,12 +59,12 @@ for filename in os.listdir(input_folder):
                 try:
                     time.sleep(random.uniform(0.1, 0.3))
                     with requests.get(file_url, stream=True, timeout=10) as r:
-                        version['file_size'] = int(r.headers.get('Content-Length', 0))
+                        version['Fs'] = int(r.headers.get('Content-Length', 0))
                         first_bytes = r.raw.read(33600)
                         with open(temp_bin, "wb") as temp_file:
                             temp_file.write(first_bytes)
 
-                    version['spiffs'] = False
+                    version['s'] = False
                     if os.path.getsize(temp_bin) > 33120:
                         with open(temp_bin, "rb") as temp_file:
                             temp_file.seek(0x8000)
@@ -75,13 +75,13 @@ for filename in os.listdir(input_folder):
                                     app_size_bytes = temp_file.read(16)
                                     if (app_size_bytes[3] in (0x00, 0x20, 0x10)) and app_size_bytes[6] == 0x01:
                                         calc_size = app_size_bytes[0x0A] << 16 | app_size_bytes[0x0B] << 8 | 0x00
-                                        version['app_size'] = min(calc_size, int(r.headers.get('Content-Length', 0)) - 0x10000)
+                                        version['as'] = min(calc_size, int(r.headers.get('Content-Length', 0)) - 0x10000)
                                     elif app_size_bytes[3] == 0x82:
-                                        version['spiffs_size'] = app_size_bytes[0x0A] << 16 | app_size_bytes[0x0B] << 8 | 0x00
-                                        version['spiffs_offset'] = app_size_bytes[0x06] << 16 | app_size_bytes[0x07] << 8 | app_size_bytes[0x08]
-                                        version['spiffs'] = version['file_size'] >= version['spiffs_offset'] + version['spiffs_size']
+                                        version['ss'] = app_size_bytes[0x0A] << 16 | app_size_bytes[0x0B] << 8 | 0x00
+                                        version['so'] = app_size_bytes[0x06] << 16 | app_size_bytes[0x07] << 8 | app_size_bytes[0x08]
+                                        version['s'] = version['Fs'] >= version['so'] + version['ss']
                             else:
-                                version['app_size'] = int(r.headers.get('Content-Length', 0))
+                                version['as'] = int(r.headers.get('Content-Length', 0))
                                 version['nb'] = True
                 except Exception as e:
                     print(f"Erro ao processar {file_url}: {e}")
